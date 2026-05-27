05a7b7c4ddda8913eb4c71141d39b3bd0e262bb5
Gallahard <munarok22@gmail.com>
2026-05-27 14:58:51 +0700

    change: change tabs data structure and change conditional rendering strusture

--- src/components/NavigationBar/TabManagement.tsx
+++ src/components/NavigationBar/TabManagement.tsx
@@ -2,32 +2,34 @@ import { Index, JSX, Show } from "solid-js";
 
 export default function TabManagement (): JSX.Element
 {
-    // Bahan untuk di loop dari 1 - 3
-    const tabs: string[] = ["About Me", "Logo's", "Project List"]
+    const tabs = Array.of(
+        { name: "About Me", isActive: true },
+        { name: "Project List", isActive: false}
+    );
 
     return (
         <>
             <Index each={tabs}>
-                {(v, i) => 
+                {(t, i) => 
                     (
-                        <Show
-                            when={i === 1}
-                            fallback={
-                                <a
-                                    href={v()}
-                                    class='no-underline font-sans text-sm antialiased text-[#6EACDA]'
-                                >
-                                    {v()}
-                                </a>
-                            }
-                        >
-                            <div class='flex flex-row items-center justify-center space-x-6'>
-                                <h4 class='font-montserrat font-extrabold antialiased text-[#E2E2B6]'>
-                                    FOLIO
-                                    <span class='text-[#6EACDA]'>ONE</span>
-                                </h4>
-                            </div>
-                        </Show>
+                        <>
+                            <h6
+                                class='no-underline font-sans text-sm antialiased text-[#6EACDA] hover:text-[#E2E2B6] transition-colors duration-300'
+                                classList={{"text-[#E2E2B6]": t().isActive}}
+                                
+                            >
+                                {t().name}
+                            </h6>
+
+                            <Show when={i === 0}>
+                                <div class='flex flex-row items-center justify-center space-x-6'>
+                                    <h4 class='font-montserrat font-extrabold antialiased text-[#E2E2B6]'>
+                                        FOLIO
+                                        <span class='text-[#6EACDA]'>ONE</span>
+                                    </h4>
+                                </div>
+                            </Show>
+                        </>
                     )
                 }
             </Index>
